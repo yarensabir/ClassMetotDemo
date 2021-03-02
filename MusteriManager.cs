08e81e210d2dc0085a507a1c35d0bce8b5fc201c
yarensabir <yaren.s.ae@gmail.com>
2021-03-02 21:34:10 +0300

    Proje dosyası ekle.

---
+++ MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        char cevap;
+        public void MusteriEkle(Musteri newMusteri)
+        {
+            Console.WriteLine(newMusteri.Ad +" "+ newMusteri.Soyad +" isimli musteri kaydi basariyla tamamlanmıstır.");
+        }
+        public void MusteriSil(Musteri newMusteri)
+        {
+            Console.WriteLine(newMusteri.Ad + " " + newMusteri.Soyad + " isimli musterimizin kaydi silinmistir.");
+        }
+        //müşteri listeleme metodu eksik
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Engin Demiroğ - Yazılım Geliştirici Yetiştirme Kampı
+// Gün 3 - Ödev 3
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Papiş";
+            musteri1.Soyad = "Muhabbetgil";
+            musteri1.ID = "0001";
+            musteri1.yas = "0.6";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Miço";
+            musteri2.Soyad = "Muhabbetgil";
+            musteri2.ID = "001";
+            musteri2.yas = "4";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Tospik";
+            musteri3.Soyad = "Tospağagil";
+            musteri3.ID = "0002";
+            musteri3.yas = "100";
+
+            MusteriManager MusteriIslemleri = new MusteriManager();
+            MusteriIslemleri.MusteriEkle(musteri1);
+            MusteriIslemleri.MusteriEkle(musteri2);
+            MusteriIslemleri.MusteriEkle(musteri3);
+            MusteriIslemleri.MusteriSil(musteri2);
+
+
+        }
+    }
+}
